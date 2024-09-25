0cf1df3c05d9298f95a7494e5cd82f1441a3c2a5
Carlyn Pukk <carlyn@Carlyns-MacBook-Air.local>
2024-09-25 18:12:47 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string=sõne
+string userName;
+
+userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
